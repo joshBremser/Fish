2f7277aab4ac3ca728abc3b70847d8e7b302ed07
Josh <joshua.bremser@maine.edu>
2020-05-04 18:26:25 -0400

    Move files to correct location

---
+++ Fish/Fish/Views/MyFish1Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Fish.Views
+{
+    public partial class MyFish1Page : ContentPage
+    {
+        public MyFish1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
