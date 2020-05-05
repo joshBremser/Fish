53ffe6a6b84dc3373463bf0c82daf47d990683da
Taylor Wilk <60187421+taylorwilk@users.noreply.github.com>
2020-05-04 21:26:58 -0400

    Add files via upload
    Comment1Page is empty as of now. Just wanted to have the comment button link to something.

---
+++ Comment1Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Fish.Views
+{
+    public partial class Comment1Page : ContentPage
+    {
+        public Comment1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
