3f2fc174005d441f8d3ed417d07f89b923b9bf0b
bdurka <60187679+bdurka@users.noreply.github.com>
2020-05-05 10:26:42 -0400

    Add files via upload
    add a new fish tracker via main fishing map page

---
+++ NewItemPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Fish.Models;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fish.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class NewItemPage : ContentPage
+    {
+        private object image;
+       
+
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+
+            Item = new Item
+            {
+                Text = "Item name",
+                Description = "This is an item description."
+            };
+
+            BindingContext = this;
+        }
+
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "AddItem", Item);
+            await Navigation.PushAsync(new MapMasterDetail());
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void OnPickPhotoButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).IsEnabled = false;
+
+            System.IO.Stream stream = await DependencyService.Get<IPhotoPickerService>().GetImageStreamAsync();
+            if (stream != null)
+            {
+               image = ImageSource.FromStream(() => stream);
+            }
+
+    (sender as Button).IsEnabled = true;
+        }
+    }
+
+    internal interface IPhotoPickerService
+    {
+        Task<Stream> GetImageStreamAsync();
+    }
+}
