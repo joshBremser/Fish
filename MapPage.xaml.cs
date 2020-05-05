629156cb26c16384b6c68d7ca5d73cc845b6fcf7
Taylor Wilk <60187421+taylorwilk@users.noreply.github.com>
2020-05-04 22:07:51 -0400

    Add files via upload
    Added another MyFish page.  This ones for the fish icon all the way on the right of the MapPage

---
+++ MapPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Fish.Views
+{
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+            BackgroundImageSource = "sebagolake.png";
+        }
+
+        async void TapGestureRecognizer_Tapped(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new MyFish1Page());
+        }
+
+        async void TapGestureRecognizer_Tapped_1(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new MyFish6Page());
+        }
+    }
+}
---
+++ MyFish6Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Fish.Views
+{
+    public partial class MyFish6Page : ContentPage
+    {
+        public MyFish6Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
